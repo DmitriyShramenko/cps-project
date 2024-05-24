43f2e16d46e254d1ee11d68c1f3e0b3c84a13738
Dmitriy Shramenko <dimdim89dimdim89@gmail.com>
2024-05-24 18:56:34 +0500

    brand-list

---
+++ brand list/script.js
@@ -0,0 +1,34 @@
+let button = document.querySelector('.brand-button');
+const allElements = document.querySelectorAll('.brand-list__elements');
+const hiddenElements = document.querySelectorAll('.brand-list__elements.hidden');
+
+button.addEventListener('click', function () {
+	if (button.textContent === 'Показать все') {
+		hiddenElements.forEach(element => {
+			element.classList.remove('hidden');
+		});
+		button.textContent = 'Скрыть';
+		button.classList.add('hide-button');
+	} else {
+		hiddenElements.forEach(element => {
+			element.classList.add('hidden');
+		});
+		button.textContent = 'Показать все';
+		button.classList.remove('hide-button');
+	}
+});
+
+new Swiper('.swiper', {
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	spaceBetween: 16,
+});
+
+
+
+
+
+
+
