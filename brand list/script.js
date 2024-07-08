58acc58a65080181aa3b2fe2e480c4805dd0c5f2
Dmitriy Shramenko <dimdim89dimdim89@gmail.com>
2024-07-08 19:56:09 +0500

    editing before assembly

--- brand list/script.js
+++ brand list/script.js
@@ -1,20 +1,37 @@
-let button = document.querySelector('.brand-button');
-const allElements = document.querySelectorAll('.brand-list__elements');
-const hiddenElements = document.querySelectorAll('.brand-list__elements.hidden');
+let button1 = document.querySelector('.brand-button1');
+let button2 = document.querySelector('.brand-button2');
+const hiddenElements1 = document.querySelectorAll('.brand-list__elements.hidden1');
+const hiddenElements2 = document.querySelectorAll('.brand-list__elements.hidden2');
 
-button.addEventListener('click', function () {
-	if (button.textContent === 'Показать все') {
-		hiddenElements.forEach(element => {
-			element.classList.remove('hidden');
+button1.addEventListener('click', function () {
+	if (button1.textContent === 'Показать все') {
+		hiddenElements1.forEach(element => {
+			element.classList.remove('hidden1');
 		});
-		button.textContent = 'Скрыть';
-		button.classList.add('hide-button');
+		button1.textContent = 'Скрыть';
+		button1.classList.add('hide-button');
 	} else {
-		hiddenElements.forEach(element => {
-			element.classList.add('hidden');
+		hiddenElements1.forEach(element => {
+			element.classList.add('hidden1');
 		});
-		button.textContent = 'Показать все';
-		button.classList.remove('hide-button');
+		button1.textContent = 'Показать все';
+		button1.classList.remove('hide-button');
+	}
+});
+
+button2.addEventListener('click', function () {
+	if (button2.textContent === 'Показать все') {
+		hiddenElements2.forEach(element => {
+			element.classList.remove('hidden2');
+		});
+		button2.textContent = 'Скрыть';
+		button2.classList.add('hide-button');
+	} else {
+		hiddenElements2.forEach(element => {
+			element.classList.add('hidden2');
+		});
+		button2.textContent = 'Показать все';
+		button2.classList.remove('hide-button');
 	}
 });
 
